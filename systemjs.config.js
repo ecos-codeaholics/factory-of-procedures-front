49730c5de115a3eda19267bf9b25401c0d26ee23
snaphuman <fhernandezn@gmail.com>
2016-07-16 18:02:48 -0500

    [app] initial commit with config and project definition

---
+++ systemjs.config.js
@@ -0,0 +1,45 @@
+(function(global) {
+
+    var map = {
+	'app': 'app',
+	'@angular': 'node_modules/@angular',
+	'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+	'rxjs': 'node_modules/rxjs'
+	
+    };
+
+    var packages = {
+	'app': { main: 'main.js', defaultExtension: 'js' },
+	'rxjs': { defaultExtension: 'js' },
+	'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js0' }
+    };
+
+    var ngPackageNames = [
+	'common',
+	'compiler',
+	'core',
+	'forms',
+	'http',
+	'platform-browser',
+	'platform-browser-dynamic',
+	'router',
+	'router-deprecated',
+	'upgrade'
+    ];
+
+    function packIndex(pkgName) {
+	packages['@angular/'+pkgName] = { main : '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    }
+
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+    ngPackageNames.forEach(setPackageConfig);
+
+    var config = {
+	map: map,
+	packages: packages
+    }
+
+    System.config(config);
+    
+})(this);
