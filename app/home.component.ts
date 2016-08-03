e2b7cc7e7995127c66c0e8be4a0524c99a4ab212
snaphuman <fhernandezn@gmail.com>
2016-08-03 12:25:50 -0500

    [fix] make use of router directives to create a simple link from home component

--- app/home.component.ts
+++ app/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
 
 @Component({
     selector: 'app-home',
     templateUrl: 'app/templates/home.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeComponent {
