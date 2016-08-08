6f5a346baed2c1f73f109f95487d6f991668510a
snaphuman <fhernandezn@gmail.com>
2016-08-08 12:19:12 -0500

    [citizen] initialize selection of procedure component with basic view

---
+++ app/citizen/citizen-select-procedure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'select-procedure',
+    templateUrl: 'app/citizen/templates/citizen-select-procedure.html'
+})
+
+export class CitizenSelectProcedureComponent {
+    title = 'Seleccione el trámite que desea realizar';
+}
--- app/home.component.ts
+++ app/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { CitizenSelectProcedureComponent } from './citizen/citizen-select-procedure.component';
 
 
 @Component({
     selector: 'app-home',
     templateUrl: 'app/templates/home.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, CitizenSelectProcedureComponent],
+    precompile: [CitizenSelectProcedureComponent]
 })
 
 export class HomeComponent {
