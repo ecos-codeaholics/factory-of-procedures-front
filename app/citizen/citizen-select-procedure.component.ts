6cb1239e8ac81d0f4273d189b93999471e224ce5
snaphuman <fhernandezn@gmail.com>
2016-08-08 22:28:33 -0500

    [citizen] filter cities and procedures by user selection in dashboard closes #1

--- app/citizen/citizen-select-procedure.component.ts
+++ app/citizen/citizen-select-procedure.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { State } from '../state/state';
 import { StateService } from '../state/state.service';
 
+
 @Component({
     selector: 'select-procedure',
     templateUrl: 'app/citizen/templates/citizen-select-procedure.html',
@@ -14,6 +15,13 @@ export class CitizenSelectProcedureComponent implements OnInit {
     title = 'Seleccione el trámite que desea realizar';
 
     states: State[];
+    state: any;
+    city: any;
+
+    cities: Array<string>;
+    procedures: Array<string>;
+
+    selectedElement: string;
 
     constructor(
         private stateService: StateService
@@ -23,6 +31,18 @@ export class CitizenSelectProcedureComponent implements OnInit {
 
         this.stateService.getStates()
             .then(states => this.states = states);
+    }
+
+    getCities() {
+        this.cities = this.states[this.state].cities;
+    }
+
+    // TODO: To be completed with stored procedures.
+    // Meanwhile, procedures variable will contain the city
+    // name to handle conditional rendering of form-control
+    // element and it's childs
+    getProcedures() {
+        this.procedures = this.city;
     }
 
     ngOnInit() {
