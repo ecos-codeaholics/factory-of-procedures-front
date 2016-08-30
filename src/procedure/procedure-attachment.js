ffcc8041fa7664bc1b25b685777ad6381e53ca1b
snaphuman <fhernandezn@gmail.com>
2016-08-29 20:08:41 -0500

    [cleanup] remove transpiled files

--- src/procedure/procedure-attachment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var ProcedureAttachment = (function () {
-    function ProcedureAttachment(name) {
-        this.name = name;
-    }
-    return ProcedureAttachment;
-}());
-exports.ProcedureAttachment = ProcedureAttachment;
-//# sourceMappingURL=procedure-attachment.js.map
