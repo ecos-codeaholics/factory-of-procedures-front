066e0dc941919ddd8c1596345ab691919269b673
snaphuman <fhernandezn@gmail.com>
2016-08-31 12:48:07 -0500

    [fix] missing typo

--- src/shared/constant/api-url.ts
+++ src/shared/constant/api-url.ts
@@ -2,5 +2,5 @@
 export class API_URL {
 
     public static get SESSIONS(): string { return 'http://127.0.0.1:4567/sessions/'; }
-    public static get CITIZENS(): string { return 'http://127.0.0.1:4567/citizen/'; }
+    public static get CITIZENS(): string { return 'http://127.0.0.1:4567/citizens/'; }
 }
