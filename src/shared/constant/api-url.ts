1ac69bcc1ef1f6945a1e9717f948c63131c0d0fa
jasonlll88 <jasonlll88@hotmail.com>
2016-09-17 10:30:13 -0500

    [merge] fix recover module

--- src/shared/constant/api-url.ts
+++ src/shared/constant/api-url.ts
@@ -5,7 +5,7 @@ export class API_URL {
     //Andres
     //public static get SESSIONS(): string { return 'http://157.253.221.90:4567/sessions/'; }
     //public static get CITIZENS(): string { return 'http://157.253.221.90:4567/citizens/'; }
-    
+
     //Jeison
  //   public static get SESSIONS(): string { return 'http://157.253.17.148:4567/sessions/'; }
  //   public static get CITIZENS(): string { return 'http://157.253.17.148:4567/citizens/'; }
