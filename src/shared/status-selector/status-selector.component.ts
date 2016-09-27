6f819ba0d4db611099a5f93d1063d352899185c1
JheisonRodriguez <jasonlll88@hotmail.com>
2016-09-27 18:05:20 -0500

    [fix] https://github.com/ecos-codeaholics/factory-of-procedures-front/issues/21

--- src/shared/status-selector/status-selector.component.ts
+++ src/shared/status-selector/status-selector.component.ts
@@ -3,15 +3,15 @@ import { Component, Output, EventEmitter, OnInit } from '@angular/core/';
 @Component({
     selector: 'status-selector',
     templateUrl: 'src/shared/status-selector/templates/status-selector.component.html'
+
 })
 
 export class StatusSelectorComponent implements OnInit {
     @Output() select = new EventEmitter();
 
-    statuses = ['ongoing', 'all'];
+    statuses = ['en proceso', 'finalizado'];
 
     ngOnInit() {
-
         this.select.emit(this.statuses[1]);
     }
 }
