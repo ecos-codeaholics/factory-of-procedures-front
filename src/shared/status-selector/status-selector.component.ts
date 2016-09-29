b5f9b7810cb1a41dc7af0ef2a04eef2e94ab4bac
sebascardonac11 <sebascardonac11@gmail.com>
2016-09-28 23:51:44 -0500

    [F21][SCC] Ajustando al mockUP

--- src/shared/status-selector/status-selector.component.ts
+++ src/shared/status-selector/status-selector.component.ts
@@ -8,9 +8,13 @@ import { Component, Output, EventEmitter, OnInit } from '@angular/core/';
 
 export class StatusSelectorComponent implements OnInit {
     @Output() select = new EventEmitter();
+    statuses = ['En proceso', 'Finalizado'];
 
-    statuses = ['en proceso', 'finalizado'];
-
+    filterHistoric(status) {
+        let val=this.statuses.indexOf(status);
+        if (val != -1)
+        this.select.emit(this.statuses[val]);
+    }
     ngOnInit() {
         this.select.emit(this.statuses[1]);
     }
