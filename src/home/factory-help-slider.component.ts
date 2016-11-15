3a0f51111a63d5d91e993ca85d13c9b3efec8f52
snaphuman <fhernandezn@gmail.com>
2016-11-14 19:31:36 -0500

    [theme] add style to help slider, custom background and some fixes

--- src/home/factory-help-slider.component.ts
+++ src/home/factory-help-slider.component.ts
@@ -20,35 +20,34 @@ export class FactoryHelpSliderComponent implements OnInit {
 
         this.slides.push({
             step: 1,
-            icon: 'http://placehold.it/555x320',
+            icon: 'http://placehold.it/555x290',
             text: 'Crea una cuenta de usuario',
         });
 
         this.slides.push({
             step: 2,
-            icon: 'http://placehold.it/555x320/c3c3c9',
+            icon: 'http://placehold.it/555x290/c3c3c9',
             text: 'Ingresa a la plataforma'
         });
 
         this.slides.push({
             step: 3,
-            icon: 'http://placehold.it/555x320/f5c349',
+            icon: 'http://placehold.it/555x290/f5c349',
             text: 'Selecciona el trámite de tu alcaldía'
         });
 
         this.slides.push({
             step: 4,
-            icon: 'http://placehold.it/555x320/c3f5d2',
+            icon: 'http://placehold.it/555x290/c3f5d2',
             text: 'Diligencia el formulario'
         });
 
         this.slides.push({
             step: 5,
-            icon: 'http://placehold.it/555x320/c3f5d2',
+            icon: 'http://placehold.it/555x290/c3f5d2',
             text: 'Sube la documentacion requerida'
         })
     }
-
 
     ngOnInit() {
         return this.setSliderContent();
