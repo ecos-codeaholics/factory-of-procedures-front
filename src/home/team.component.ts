ea272bd1549c139bcb7118d9e290728366fc7c8e
snaphuman <fhernandezn@gmail.com>
2016-11-20 18:58:48 -0500

    [theme] add content and layout to informative pages

--- src/home/team.component.ts
+++ src/home/team.component.ts
@@ -4,16 +4,17 @@
 
 import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
     selector: 'team-tab',
-    templateUrl:  'src/home/templates/team.component.html'
+    templateUrl: 'src/home/templates/team.component.html'
 })
 
 export class TeamComponent {
 
+    title = "El equipo";
 
 }
 
