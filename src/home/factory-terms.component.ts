1b2747ac51b5201ce976fabc55ec1cc0360a5351
snaphuman <fhernandezn@gmail.com>
2016-11-26 09:57:07 -0500

    [theme] add terms and conditions page closes #32

---
+++ src/home/factory-terms.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'app-terms',
+    templateUrl: 'src/home/templates/factory-terms.component.html'
+})
+
+export class FactoryTermsComponent {
+
+    termsTitle = 'Términos y Condiciones de Uso';
+
+    politicsTitle = 'Políticas de Privacidad';
+}
